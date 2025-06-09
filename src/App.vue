9ed8a447f364bb73685e3ff20b9c4d7e3fd72633
Nova Novriansyah <nov_rian@yahoo.com>
2025-06-09 18:12:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <h1>Pinia Counter App</h1>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment">+</button>
+    <button @click="counter.decrement">-</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from './stores/counter'
+const counter = useCounterStore()
+</script>
+
+<style>
+.app {
+  text-align: center;
+  font-family: sans-serif;
+  padding: 2rem;
+}
+button {
+  margin: 0.5rem;
+  padding: 0.5rem 1rem;
+}
+</style>
